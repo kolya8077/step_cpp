0621a5761c3c30ad4cedf54e02189bbef7827d79
kolya8077 <kolya8077@gmail.com>
2024-03-08 10:43:21 +0200

    Add project files.

---
+++ step_cpp/lesson_1_1.cpp
@@ -0,0 +1,38 @@
+//#include <iostream>
+//using namespace std;
+//
+//
+//void main()
+//{
+//    //1.
+//    //    I
+//    //        love
+//    //            you
+//    //                C++!
+//
+//    cout << "I\n\tlove\n\t\tyou\n\t\t\tc++\n";
+//
+//    //…:::RESUME:: : …
+//
+//    //    Name: Ivan
+//    //    Surname : Ivanov
+//    //    Last name : Ivanovich
+//
+//    //    Date of birth : 22.06.06
+//    //    City : Rivne
+//    //    Age : 17
+//
+//    //    Hobby : C++
+//    //    ::……………………...................……::
+//
+//    cout << "\n\t…:::RESUME:::…\n\n";
+//    cout << "Name:\t\t\tIvan\n";
+//    cout << "Last name: \t\tIvanovich\n\n";
+//    cout << "Data of birth:\t\t22.06.06\n";
+//    cout << "City:\t\t\tRivne\n";
+//    cout << "Age:\t\t\t17\n\n";
+//    cout << "Hobby:\t\t\tC++\n";
+//    cout << "::..............................::";
+//
+//}
+//
---
+++ step_cpp/lesson_5.cpp
@@ -0,0 +1,44 @@
+//#include <iostream>
+//#include<windows.h>
+//
+//using namespace std;
+//
+//
+//int main()
+//{
+//    SetConsoleCP(1251);
+//    SetConsoleOutputCP(1251);
+//
+//    /*a*/
+//
+//    int N = 7;
+//    for (int i = 0; i < 17; i++) 
+//    {
+//        cout << "_";
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//}
