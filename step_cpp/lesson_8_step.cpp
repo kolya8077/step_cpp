4b48d3b916bfb32ab8cc43786a52ab67bfd0bfc5
kolya8077 <kolya8077@gmail.com>
2024-03-19 09:05:12 +0200

    hw 8_1

---
+++ step_cpp/lesson_8_step.cpp
@@ -0,0 +1,61 @@
+//#include <iostream>
+//#include <windows.h>
+////#include <time.h>
+////#include <stdlib.h>
+//
+////#include <ctime>
+//
+//using namespace std;
+//
+//
+//
+//void Hello()
+//{
+//	cout << "Hello world ! " << endl;
+//}
+//void Star(int count)
+//{
+//	for (int i = 0; i < count; i++)
+//	{
+//		cout << "*" << " ";
+//	}
+//	cout << endl;
+//}
+//void AnyLine(char symbol, int count)
+//{
+//	for (int i = 0; i < count; i++)
+//	{
+//		cout << symbol << " ";
+//	}cout << endl;
+//}
+//
+//void print(string x = "", string y = "") {
+//	cout << x << y;
+//}
+//
+//void les_1(int row, int col, string sumbol = "$");
+//
+//
+//int main()
+//{
+//    SetConsoleCP(1251);
+//    SetConsoleOutputCP(1251);
+//
+//    srand(time(NULL));
+//	les_1(5, 5);
+//
+//
+//}
+//
+//
+//void les_1(int row, int col, string sumbol) 
+//{
+//	for (int i = 0; i < row; i++)
+//	{
+//		for (int j = 0; j < col; j++)
+//		{
+//			print(sumbol, " ");
+//		}
+//		print();
+//	}
+//}
