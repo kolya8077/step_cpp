5cb4c80f1d882bccf312a762ed980a5a78f7a7dd
kolya8077 <kolya8077@gmail.com>
2024-03-27 21:44:05 +0200

    hw les 14

--- step_cpp/lesson_11_1.cpp
+++ step_cpp/lesson_11_1.cpp
@@ -1,166 +1,166 @@
-#include <iostream>
-#include <windows.h>
-
-using namespace std;
-
-template<typename T>
-void print(T x = "", string y = " ");
-void end();
-
-void les_1();
-void les_2();
-void les_3();
-void les_4();
-void les_5();
-
-int main()
-{
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    srand(time(0));
-
-    //les_1();
-    les_2();
-    //les_3();
-    //les_4();
-    //les_5();
-}
-
-
-template<typename T>
-inline void print(T x, string y) {
-    cout << x << y;
-}
-
-inline void end() {
-    cout << endl;
-}
-
-/*
-Завдання 1. Використовуючи покажчики й оператор
-розіменування, визначити найбільше з двох чисел.
-*/
-
-void max_num(int pa, int pb, int& pmax) {
-    (pa > pb) ? pmax = pa : pmax = pb;
-}
-
-void les_1() {
-    int a, b, max;
-    print("Введіть числа для порівняння:");
-    cin >> a >> b;
-    int* pa = &a;
-    int* pb = &b;
-    int* pmax = &max;
-    max_num(*pa, *pb, *pmax);
-    print(max);
-}
-
-/*
-Завдання 2. Використовуючи покажчики й оператор розі-
-менування, визначити знак числа, введеного з клавіатури.
-*/
-
-void sing(int num) {
-    if (num > 0) return print("positive");
-    else if (num < 0) return print("negative");
-    else if (num == 0) return print("zero");
-}
-
-void les_2() {
-    int num;
-    int* pnum = &num;
-    print("Enter number:");
-    cin >> num;
-    sing(*pnum);
-
-}
-
-/*
-Завдання 3. Використовуючи покажчики й оператор роз-
-іменування, поміняти місцями значення двох змінних.
-*/
-
-void revers_num(int& pa, int& pb) {
-    int temp = pa;
-    pa = pb;
-    pb = temp;
-}
-
-void les_3() {
-    int a, b;
-    print("Введіть числа:");
-    cin >> a >> b;
-    int* pa = &a;
-    int* pb = &b;
-    revers_num(*pa, *pb);
-    print(a);
-    print(b);
-}
-
-/*
-Завдання 4. Написати примітивний калькулятор, кори-
-стуючись тільки покажчиками.
-*/
-
-void calc(int& pa, int& pb, char& pc, int& res) {
-    if (pc == '*') {
-        res = pa * pb;
-    }else if (pc == '/') {
-        res = pa / pb;
-    }else if (pc == '+') {
-        res = pa + pb;
-    }else if (pc == '-') {
-        res = pa - pb;
-    }
-}
-
-void les_4() {
-    int a, b, res;
-    char c;
-
-    print("calc:");
-    cin >> a >> c >> b ;
-    print('=');
-    
-    int* pa = &a;
-    int* pb = &b;
-    char* pc = &c;
-    int* pres = &res;
-
-    calc(*pa, *pb, *pc, *pres);
-    print(res);
-
-}
-
-/*
-Завдання 5. Використовуючи покажчик на масив цілих
-чисел, порахувати суму елементів масиву. Використову-
-вати в програмі арифметику покажчиків для просування
-по масиву, а також оператор розіменування.
-*/
-
-
-
-void les_5() {
-    const int size = 10;
-    int arr[size]{};
-    int* parr = &arr[0];
-
-    int sum = 0;
-
-    for (int i = 0; i < size; i++)
-    {
-        *(parr + i) = rand() % 10;
-    }
-    for (int i = 0; i < size; i++)
-    {
-        print(*(parr + i));
-    }
-    for (int i = 0; i < size; i++)
-    {
-        sum += *(parr + i);
-    }
-    end();
-    print(sum);
-}
+//#include <iostream>
+//#include <windows.h>
+//
+//using namespace std;
+//
+//template<typename T>
+//void print(T x = "", string y = " ");
+//void end();
+//
+//void les_1();
+//void les_2();
+//void les_3();
+//void les_4();
+//void les_5();
+//
+//int main()
+//{
+//    SetConsoleCP(1251);
+//    SetConsoleOutputCP(1251);
+//    srand(time(0));
+//
+//    //les_1();
+//    les_2();
+//    //les_3();
+//    //les_4();
+//    //les_5();
+//}
+//
+//
+//template<typename T>
+//inline void print(T x, string y) {
+//    cout << x << y;
+//}
+//
+//inline void end() {
+//    cout << endl;
+//}
+//
+///*
+//Завдання 1. Використовуючи покажчики й оператор
+//розіменування, визначити найбільше з двох чисел.
+//*/
+//
+//void max_num(int pa, int pb, int& pmax) {
+//    (pa > pb) ? pmax = pa : pmax = pb;
+//}
+//
+//void les_1() {
+//    int a, b, max;
+//    print("Введіть числа для порівняння:");
+//    cin >> a >> b;
+//    int* pa = &a;
+//    int* pb = &b;
+//    int* pmax = &max;
+//    max_num(*pa, *pb, *pmax);
+//    print(max);
+//}
+//
+///*
+//Завдання 2. Використовуючи покажчики й оператор розі-
+//менування, визначити знак числа, введеного з клавіатури.
+//*/
+//
+//void sing(int num) {
+//    if (num > 0) return print("positive");
+//    else if (num < 0) return print("negative");
+//    else if (num == 0) return print("zero");
+//}
+//
+//void les_2() {
+//    int num;
+//    int* pnum = &num;
+//    print("Enter number:");
+//    cin >> num;
+//    sing(*pnum);
+//
+//}
+//
+///*
+//Завдання 3. Використовуючи покажчики й оператор роз-
+//іменування, поміняти місцями значення двох змінних.
+//*/
+//
+//void revers_num(int& pa, int& pb) {
+//    int temp = pa;
+//    pa = pb;
+//    pb = temp;
+//}
+//
+//void les_3() {
+//    int a, b;
+//    print("Введіть числа:");
+//    cin >> a >> b;
+//    int* pa = &a;
+//    int* pb = &b;
+//    revers_num(*pa, *pb);
+//    print(a);
+//    print(b);
+//}
+//
+///*
+//Завдання 4. Написати примітивний калькулятор, кори-
+//стуючись тільки покажчиками.
+//*/
+//
+//void calc(int& pa, int& pb, char& pc, int& res) {
+//    if (pc == '*') {
+//        res = pa * pb;
+//    }else if (pc == '/') {
+//        res = pa / pb;
+//    }else if (pc == '+') {
+//        res = pa + pb;
+//    }else if (pc == '-') {
+//        res = pa - pb;
+//    }
+//}
+//
+//void les_4() {
+//    int a, b, res;
+//    char c;
+//
+//    print("calc:");
+//    cin >> a >> c >> b ;
+//    print('=');
+//    
+//    int* pa = &a;
+//    int* pb = &b;
+//    char* pc = &c;
+//    int* pres = &res;
+//
+//    calc(*pa, *pb, *pc, *pres);
+//    print(res);
+//
+//}
+//
+///*
+//Завдання 5. Використовуючи покажчик на масив цілих
+//чисел, порахувати суму елементів масиву. Використову-
+//вати в програмі арифметику покажчиків для просування
+//по масиву, а також оператор розіменування.
+//*/
+//
+//
+//
+//void les_5() {
+//    const int size = 10;
+//    int arr[size]{};
+//    int* parr = &arr[0];
+//
+//    int sum = 0;
+//
+//    for (int i = 0; i < size; i++)
+//    {
+//        *(parr + i) = rand() % 10;
+//    }
+//    for (int i = 0; i < size; i++)
+//    {
+//        print(*(parr + i));
+//    }
+//    for (int i = 0; i < size; i++)
+//    {
+//        sum += *(parr + i);
+//    }
+//    end();
+//    print(sum);
+//}
--- step_cpp/lesson_14_step.cpp
+++ step_cpp/lesson_14_step.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include <iomanip>
 
 using namespace std;
 
@@ -7,7 +8,7 @@ template<typename T>
 void print(T x = "", string y = " ");
 void end();
 
-
+void step();
 
 int main()
 {
@@ -15,7 +16,7 @@ int main()
     SetConsoleOutputCP(1251);
     srand(time(0));
 
-
+    step();
 }
 
 
@@ -26,4 +27,260 @@ inline void print(T x, string y) {
 
 inline void end() {
     cout << endl;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+void FillArr(int** arr, int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 20 + 10;
+		}
+	}
+}
+void ShowArr(int** arr, int rows, int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << setw(3) << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void FillRow(int* arr, int cols)
+{
+	for (int i = 0; i < cols; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+int** AddRowInTheEnd(int** arr, int& rows, int cols)
+{
+	rows++;
+	int** temp = new int* [rows];
+	for (int i = 0; i < rows - 1; i++)
+	{
+		temp[i] = arr[i];
+	}
+	temp[rows - 1] = new int[cols];
+	FillRow(temp[rows - 1], cols);
+	delete[]arr;
+	return temp;
+}
+void DeleteRowInTheEnd(int**& arr, int& rows, int cols)
+{
+	int** temp = new int* [rows - 1];
+	for (int i = 0; i < rows - 1; i++)
+	{
+		temp[i] = arr[i];
+	}
+	delete[] arr[rows - 1];
+	delete[]arr;
+	rows--;
+	arr = temp;
+}
+
+void AddColumnInTheEnd(int** &arr, int rows, int& cols) {
+	int** temp = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		temp[i] = new int[cols + 1];
+	}
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			temp[i][j] = arr[i][j];
+		}
+		temp[i][cols] = 7;
+	}
+	for (int i = 0; i < rows; i++)
+	{
+		delete[]arr[i];
+	}
+	delete[]arr;
+	cols++;
+	arr = temp;
+}
+
+int** AddRowInTheFirst(int** arr, int& rows, int cols)
+{
+	rows++;
+	int** temp = new int* [rows];
+	for (int i = 1; i < rows; i++)
+	{
+		temp[i] = arr[i - 1];
+	}
+	temp[0] = new int[cols];
+	FillRow(temp[0], cols);
+	delete[]arr;
+	return temp;
+}
+
+void DeleteRowInTheFirst(int**& arr, int& rows, int cols)
+{
+	int** temp = new int* [rows - 1];
+	for (int i = 0; i < rows - 1; i++)
+	{
+		temp[i] = arr[i + 1];
+	}
+	delete[] arr[0];
+	delete[]arr;
+	rows--;
+	arr = temp;
+}
+
+void DeleteRowInThePos(int**& arr, int& rows, int cols, int pos)
+{
+	int** temp = new int* [rows - 1];
+	//for (int i = 0; i < rows - 1; i++)
+	//{
+	//	temp[i] = arr[i + 1];
+	//}
+	int count = 0;
+	int index = 0;
+	while (count <= rows) {
+		if (count != pos) {
+			temp[index] = arr[count];
+			index++;
+		}
+		count++;
+	}
+
+	delete[] arr[pos];
+	delete[]arr;
+	rows--;
+	arr = temp;
+}
+
+void AddColumnInThePos(int**& arr, int rows, int& cols, int posAdd) {
+	int** temp = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		temp[i] = new int[cols + 1];
+	}
+	for (int i = 0; i < rows; i++)
+	{
+		int index = 0;
+		for (int j = 0; j <= cols; j++)
+		{
+			if (j == posAdd) temp[i][posAdd] = 7;
+			else {
+				temp[i][j] = arr[i][index];
+				index++;
+			}
+		}
+		
+	}
+	for (int i = 0; i < rows; i++)
+	{
+		delete[]arr[i];
+	}
+	delete[]arr;
+	cols++;
+	arr = temp;
+}
+
+void AddColumnInTheFirst(int**& arr, int rows, int& cols) {
+	int** temp = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		temp[i] = new int[cols + 1];
+	}
+	for (int i = 0; i < rows; i++)
+	{
+		temp[i][0] = 7;
+		for (int j = 0; j < cols; j++)
+		{
+			temp[i][j + 1] = arr[i][j];
+		}
+	}
+	for (int i = 0; i < rows; i++)
+	{
+		delete[]arr[i];
+	}
+	delete[]arr;
+	cols++;
+	arr = temp;
+}
+
+void step() {
+	int rows = 4;
+	int cols = 5;
+	//cout << "Enter count rows : ";
+	//cin >> rows;
+	//cout << "Enter count cols : ";
+	//cin >> cols;
+	int** arr = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols];
+	}
+	FillArr(arr, rows, cols);
+	ShowArr(arr, rows, cols);
+
+	arr = AddRowInTheEnd(arr, rows, cols);
+	ShowArr(arr, rows, cols);
+
+	DeleteRowInTheEnd(arr, rows, cols);
+	ShowArr(arr, rows, cols);
+
+	AddColumnInTheEnd(arr, rows, cols);
+	ShowArr(arr, rows, cols);
+
+
+	/*
+	Завдання 1. Написати функцію, що додає рядок двови-
+мірному масиву на початок.
+	*/
+
+	arr = AddRowInTheFirst(arr, rows, cols);
+	ShowArr(arr, rows, cols);
+
+	/*
+	Завдання 2. Написати функцію, що видаляє рядок двови-
+мірному масиву з початку.
+	*/
+	DeleteRowInTheFirst(arr, rows, cols);
+	ShowArr(arr, rows, cols);
+
+	/*
+	Завдання 3. Написати функцію, що видаляє рядок двови-
+мірному масиву з зазначеної позиції.
+	*/
+	int posD;
+	print("Posititon delete:");
+	cin >> posD;
+	DeleteRowInThePos(arr, rows, cols, posD);
+	ShowArr(arr, rows, cols);
+
+	/*
+	Завдання 4. Написати функцію, що додає колонку дво-
+вимірного масиву за вказаним номером.
+	*/
+	int posAdd;
+	print("Posititon add:");
+	cin >> posAdd;
+	AddColumnInThePos(arr, rows, cols, posAdd);
+	ShowArr(arr, rows, cols);
+
+	/*
+	Завдання 5. Написати функцію, що додає колонку дво-
+вимірного масиву на початок.
+	*/
+
+	AddColumnInTheFirst(arr, rows, cols);
+	ShowArr(arr, rows, cols);
+
+	for (int i = 0; i < rows; i++)
+	{
+		delete[]arr[i];
+	}
+	delete[]arr;
 }
